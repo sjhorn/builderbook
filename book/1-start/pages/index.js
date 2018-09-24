beceb1351e39a5d5c7b0c7a5141160c0348b0f37
Scott Horn <shorn@expedia.com>
2018-09-24 07:00:42 -0700

    Progress on tutorials

---
+++ book/1-start/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
+    </Head>
+    <p>Content on Index Page</p>
+  </div>
+);
+
+export default Index;
